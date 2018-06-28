1ae0bf54016ef0c32492842eff4a6e031cee414c
11ajc11 <35712981+11ajc11@users.noreply.github.com>
2018-06-28 12:54:57 -0500

    Fixed some formating

--- src/excelCompiler/Send_Post.java
+++ src/excelCompiler/Send_Post.java
@@ -12,37 +12,38 @@ import org.json.JSONObject;
 
 
 public class Send_Post {
+	
 	//takes in arguments from excelCombiner
 		
-		public static void post_me(String id, ArrayList<Integer> numberSetOne, ArrayList<Integer> numberSetTwo, ArrayList<String> wordSetOne) throws Exception {
+	public static void post_me(String id, ArrayList<Integer> numberSetOne, ArrayList<Integer> numberSetTwo, ArrayList<String> wordSetOne) throws Exception {
 			
-			//create JSON and set url
+		//create JSON and set url
 			
-			JSONObject json = new JSONObject();
-			String url = new String("http://34.239.125.159:5000/challenge");
+		JSONObject json = new JSONObject();
+		String url = new String("http://34.239.125.159:5000/challenge");
 			
-			//set the params with the id and the sets
+		//set the params with the id and the sets
 			
-			json.put("id", id);
-			json.put("numberSetOne", numberSetOne);
-			json.put("numberSetTwo", numberSetTwo);
-			json.put("wordSetOne", wordSetOne);
+		json.put("id", id);
+		json.put("numberSetOne", numberSetOne);
+		json.put("numberSetTwo", numberSetTwo);
+		json.put("wordSetOne", wordSetOne);
 			
-			System.out.println(json);
+		System.out.println(json);
 		    
-			//handles connection and post
+		//handles connection and post
 		    
-			CloseableHttpClient httpClient = HttpClients.createDefault();	    
-		    HttpPost post = new HttpPost(url);
-		    StringEntity params = new StringEntity(json.toString());
-		    post.setHeader("Accept","application/json");
-		    post.addHeader("content-type", "application/json");
-		    post.setEntity(params);
-		    CloseableHttpResponse response = httpClient.execute(post);
-		    System.out.println(response.getStatusLine().toString());
-		    httpClient.close();
-		    response.close();
-		    }
+		CloseableHttpClient httpClient = HttpClients.createDefault();	    
+		HttpPost post = new HttpPost(url);
+		StringEntity params = new StringEntity(json.toString());
+		post.setHeader("Accept","application/json");
+		post.addHeader("content-type", "application/json");
+		post.setEntity(params);
+		CloseableHttpResponse response = httpClient.execute(post);
+		System.out.println(response.getStatusLine().toString());
+		httpClient.close();
+		response.close();
+		}
 }
 
 
